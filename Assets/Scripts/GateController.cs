0d2a24659b07ab99a39bd62712f46840b2e93450
Aylin Karadavut <aylinkrdvt1019@gmail.com>
2023-10-12 22:22:30 +0300

    ADD Gate Open Door

--- Assets/Scripts/GateController.cs
+++ Assets/Scripts/GateController.cs
@@ -5,14 +5,41 @@ using UnityEngine;
 public class GateController : MonoBehaviour
 {
     private Animator _gateAnimation;
- 
-    private void Awake()
+    private GameObject _asteroids;
+    [SerializeField] private GameObject _inActiveAsteroid;
+
+    
+
+    private void Start()
     {
         _gateAnimation = GetComponent<Animator>();
         _gateAnimation.SetBool("GateOpen", false);
+
+      
+
         
+
     }
    
+    private void Update()
+    {
+        _asteroids = GameObject.FindGameObjectWithTag("Ball");
+
+        if (_asteroids == null)
+        {
+            _gateAnimation.SetBool("GateOpen", true);
+            _inActiveAsteroid.SetActive(true);
+        }
+        //else 
+        //{
+        //    _gateAnimation.SetBool("GateOpen", false);
+           
+        //}
+    }
+
+
+
 
 
 }
+
